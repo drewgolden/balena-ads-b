d4eedc0290e9606379c70ff6798529c1ec5e2326
Ketil Moland Olsen <ketil@ketilmo.com>
2019-08-13 22:36:58 +0200

    Restructured variable verification for Plane Finder.

--- planefinder/start.sh
+++ planefinder/start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-envsubst < /etc/pfclient-config.json.tpl> /etc/pfclient-config.json
+
+# Verify that all the required varibles are set before starting up the application.
 
 missing_variables=true
 
-echo " "
 echo "Validating settings..."
 echo " "
 sleep 2
@@ -11,11 +11,15 @@ sleep 2
 while [ "$missing_variables" = true ]; do 
         missing_variables=false
         
+        # Begin defining all the required configuration variables.
+
         [ -z "$PLANEFINDER_SHARECODE" ] && echo "Plane Finder Sharecode not set, holding startup" && missing_variables=true || echo "Plane Finder Sharecode set"
         [ -z "RECEIVER_HOST" ] && echo "Receiver host not set, holding startup" && missing_variables=true || echo "Receiver host set"
         [ -z "RECEIVER_PORT" ] && echo "Receiver port not set, holding startup" && missing_variables=true || echo "Receiver port set"
         [ -z "LAT" ] && echo "Receiver latitude not set, holding startup" && missing_variables=true || echo "Receiver latitude set"
         [ -z "LON" ] && echo "Receiver longitude not set, holding startup" && missing_variables=true || echo "Receiver longitude set"
+
+        # End defining all the required configuration variables.
 
         echo " "
         
@@ -31,4 +35,7 @@ echo "Settings verified, proceeding with startup."
 echo " "
 
 
+# Variables are verified – continue with startup procedure.
+
+envsubst < /etc/pfclient-config.json.tpl> /etc/pfclient-config.json
 /usr/bin/pfclient --config_path=/etc/pfclient-config.json --log_path=/dev/console
